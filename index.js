7ae1df1afb6743c32d00ea986339e413c35fdcd0
Kevin Li <kevinli617@gmail.com>
2023-05-16 15:25:13 -0400

    added pokedex project

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const home = document.getElementById("btn-home");
 const connect4 = document.getElementById("btn-connect4")
 const calculator = document.getElementById("btn-calculator")
+const pokedex = document.getElementById("btn-pokedex")
 const iframe = document.getElementById("iframe")
 
 home.addEventListener('click', () => {
@@ -13,4 +14,8 @@ connect4.addEventListener('click', () => {
 
 calculator.addEventListener('click', () => {
     iframe.setAttribute("src", "./calculator.html")
+})
+
+pokedex.addEventListener('click', () => {
+    iframe.setAttribute("src", "./pokedex.html")
 })
