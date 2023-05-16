adda2b662b77fe6cb0432ea6c72968da42c57b2d
Kevin Li <kevinli617@gmail.com>
2023-05-15 23:09:12 -0400

    added iframe to home page, connected navbar buttons to select projects

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('javascript is running')
+const home = document.getElementById("btn-home");
+const connect4 = document.getElementById("btn-connect4")
+const calculator = document.getElementById("btn-calculator")
+const iframe = document.getElementById("iframe")
+
+home.addEventListener('click', () => {
+    iframe.setAttribute("src", "")
+})
+
+connect4.addEventListener('click', () => {
+    iframe.setAttribute("src", "./connect4.html")
+})
+
+calculator.addEventListener('click', () => {
+    iframe.setAttribute("src", "./calculator.html")
+})
