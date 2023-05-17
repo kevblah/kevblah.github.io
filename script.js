d5717dd781e86b16085d9d7b60bab140bfc7bf79
Kevin Li <kevinli617@gmail.com>
2023-05-16 23:08:37 -0400

    added new pokedex

---
+++ script.js
@@ -0,0 +1,5 @@
+// const home = document.getElementById("btn-home");
+// const connect4 = document.getElementById("btn-connect4")
+// const calculator = document.getElementById("btn-calculator")
+// const pokedex = document.getElementById("btn-pokedex")
+// const iframe = document.getElementById("iframe")
